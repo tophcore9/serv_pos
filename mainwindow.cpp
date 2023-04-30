0509639d5421be6f88071c83e938d16a071ff6ea
Alexey <alexeypazyura@gmail.com>
2023-04-30 20:04:45 +0300

    Added lists and DB connecting

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,33 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+    // БАЗОВІ НАЛАШТУВАННЯ
+    this->setWindowTitle("ServPOS");
+    this->setFixedSize(1000, 600);
+
+
+    // ДОДАВАННЯ ВІДЖЕТІВ
+    client_list = new QListView;
+    order_list = new QListView;
+    dishes_list = new QListView;
+
+
+
+    // ПІДКЛЮЧЕННЯ К БД
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("../database/main.db");
+
+    if (db.open())
+    {
+        // ГОЛОВНА ЧАСТИНА ПРОГРАМИ
+        clients = new Clients(db, this);
+
+        client_list->setModel(clients->get_model());
+    }
+    else
+    {
+        // ОБРОБКА ПОМИЛОК
+    }
 
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,10 +2,20 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "clients.h"
+#include <QtWidgets>
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+private:
+    QSqlDatabase db;
+    Clients *clients;
+
+    //GUI
+    QListView *client_list;
+    QListView *order_list;
+    QListView *dishes_list;
 
 public:
     MainWindow(QWidget *parent = 0);
