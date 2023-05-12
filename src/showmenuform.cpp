c80dd64fb686c28193972247735e7624a1d6ae8c
Alexey <alexeypazyura@gmail.com>
2023-05-12 19:08:20 +0300

    Configured edit-dishes function

--- src/showmenuform.cpp
+++ src/showmenuform.cpp
@@ -5,8 +5,27 @@ ShowMenuForm::ShowMenuForm(QModelIndex menu_index, QSqlTableModel *dishes_model,
     /// БАЗОВІ НАЛАШТУВАННЯ
     this->setFixedWidth(300);
     this->setWindowTitle(tr("Перегляд меню"));
+    this->dishes_model = dishes_model;
     past_menu_name = menu_index.data(0).toString();
     new_menu_name = past_menu_name;
+
+
+    /// ВІДЖЕТИ
+    // Додавання віджетів
+    l_name = new QLabel(tr("Назва меню:"));
+    name_edit = new QLineEdit;
+
+    l_dishes = new QLabel(tr("Страви:"));
+    add_dish_btn = new QPushButton("+");
+    add_dish_selects.push_back(new QComboBox);
+
+    accept_btn = new QPushButton(tr("Підтвердити"));
+    cancel_btn = new QPushButton(tr("Скасувати"));
+
+    // Налаштування віджетів
+    l_name->setAlignment(Qt::AlignRight);
+    name_edit->setText(past_menu_name);
+    add_dish_btn->setMaximumSize(25, 25);
 
     QSqlQuery query(dishes_model->database());
     if (query.exec("SELECT * FROM Menu"))
@@ -44,22 +63,8 @@ ShowMenuForm::ShowMenuForm(QModelIndex menu_index, QSqlTableModel *dishes_model,
                               tr("\nПовідомлення драйвера: ") + query.lastError().driverText());
     }
 
-    /// ВІДЖЕТИ
-    // Додавання віджетів
-    l_name = new QLabel(tr("Назва меню:"));
-    name_edit = new QLineEdit;
-
-    l_dishes = new QLabel(tr("Страви:"));
-    add_dish_btn = new QPushButton("+");
-    add_dish_selects.push_back(new QComboBox);
-
-    accept_btn = new QPushButton(tr("Підтвердити"));
-    cancel_btn = new QPushButton(tr("Скасувати"));
-
-    // Налаштування віджетів
-    l_name->setAlignment(Qt::AlignRight);
-    name_edit->setText(past_menu_name);
-    add_dish_btn->setMaximumSize(25, 25);
+    current_dish_item = 0;
+    dish_grid_index = 2 + current_dish_item;
 
 
     /// МАКЕТИ І КОМПОНОВКА
@@ -85,15 +90,53 @@ ShowMenuForm::ShowMenuForm(QModelIndex menu_index, QSqlTableModel *dishes_model,
     buttons_layout->addWidget(accept_btn);
     buttons_layout->addWidget(cancel_btn);
 
+    for (int i = 0; i < dishes.size(); ++i)
+    {
+        add_dish();
+        add_dish_selects[i]->setCurrentText(dishes[i]);
+    }
+
 
     /// СИГНАЛИ І СЛОТИ
     connect(accept_btn, SIGNAL(clicked()), this, SLOT(edit_menu()));
     connect(this, SIGNAL(edit_menu(QString,QString,std::vector<QString>)), parent, SLOT(edit_menu(QString,QString,std::vector<QString>)));
 
     connect(cancel_btn, SIGNAL(clicked()), this, SLOT(close()));
+
+    connect(add_dish_btn, SIGNAL(clicked()), this, SLOT(add_dish()));
 }
 
 void ShowMenuForm::edit_menu()
 {
     emit edit_menu("", "", {""});
+}
+
+void ShowMenuForm::add_dish()
+{
+    add_dish_selects.push_back(new QComboBox);
+    add_dish_selects[current_dish_item]->setModel(dishes_model);
+    add_dish_selects[current_dish_item]->setModelColumn(1);
+
+    remove_dish_btns.push_back(new QPushButton("x"));
+    remove_dish_btns[current_dish_item]->setFixedSize(25, 25);
+
+    info_layout->addWidget(add_dish_selects[current_dish_item], dish_grid_index + 1, 0);
+    info_layout->addWidget(remove_dish_btns[current_dish_item], dish_grid_index + 1, 1);
+
+    connect(remove_dish_btns[current_dish_item], &QPushButton::clicked, this, std::bind(&ShowMenuForm::remove_dish, this, current_dish_item));
+
+    ++dish_grid_index;
+    ++current_dish_item;
+}
+
+void ShowMenuForm::remove_dish(int index)
+{
+    info_layout->removeWidget(add_dish_selects[index]);
+    info_layout->removeWidget(remove_dish_btns[index]);
+
+    delete add_dish_selects[index];
+    add_dish_selects[index] = NULL;
+
+    delete remove_dish_btns[index];
+    remove_dish_btns[index] = NULL;
 }
--- src/showmenuform.h
+++ src/showmenuform.h
@@ -7,16 +7,22 @@
 #include <QSqlTableModel>
 #include <QSqlQuery>
 #include <QSqlError>
+#include <functional>
 
 class ShowMenuForm : public QDialog
 {
     Q_OBJECT
 
 private:
+    QSqlTableModel *dishes_model;
+
     QString menu_id;
     std::vector<QString> dishes;
     QString past_menu_name;
     QString new_menu_name;
+
+    int dish_grid_index;
+    int current_dish_item;
 
     /// GUI
     QVBoxLayout *main_layout;
@@ -42,6 +48,8 @@ signals:
 
 public slots:
     void edit_menu();
+    void add_dish();
+    void remove_dish(int index);
 };
 
 #endif // SHOWMENUFORM_H
